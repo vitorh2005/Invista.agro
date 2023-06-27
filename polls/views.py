47370deeaf508fa8b7c241aa5661d194eca47deb
Vitor hugo <vieiravh2005@gmail.com>
2023-06-27 10:40:08 -0300

    incluir rota sobre/index

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from polls.models import Question
 
 # Create your views here.
 
-#
-
+#View Index
 def index(request):
-    return HttpResponse('olá... seja bem vindo a enquete')
+    return HttpResponse("Olá, seja bem vindo a enquete")
+
+def sobre(request):
+    return HttpResponse('Este é um app de enquete!')
