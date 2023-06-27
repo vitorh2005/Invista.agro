abb542c7b4a70c88324bcc40e400b88437f05042
Vitor hugo <vieiravh2005@gmail.com>
2023-06-27 11:24:17 -0300

    incluir perguntas/ultimas perguntas

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.index, name="index"),
-path('sobre/', views.sobre, name='sobre'),
+    path('', views.index, name="index"),
+    path('sobre/', views.sobre, name='sobre'),
+    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao' ),
+    path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -10,3 +10,12 @@ def index(request):
 
 def sobre(request):
     return HttpResponse('Este é um app de enquete!')
+
+def exibe_questao(request, question_id):
+    questao = Question.objects.get(id=question_id)
+    return HttpResponse(questao.question_text)
+
+def ultimas_perguntas(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/perguntas.html', context)
